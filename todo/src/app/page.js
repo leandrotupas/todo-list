26b614311f020a196bb4f5776f4db16c23c33f69
leandrotupas <tupas.leandro.lt@gmail.com>
2024-03-14 12:23:34 +0800

    init

---
+++ todo/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl font-bold"> To Do List</h1>
+      <h3> By Leandro Miguel Tupas</h3>
+    </main>
+  );
+}
